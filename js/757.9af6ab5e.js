6b17f7c629137fb8138829a6c4eb174202a94ea5
Wendy <wink899@gmail.com>
2022-03-07 00:06:36 +0800

    deploy

---
+++ js/757.9af6ab5e.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue_week6"]=self["webpackChunkvue_week6"]||[]).push([[757],{5757:function(e,n,r){r.r(n),r.d(n,{default:function(){return f}});var t=r(6252);const u=(0,t._)("h2",{class:"text-center mt-5"},"訂單列表",-1),a=[u];function c(e,n,r,u,c,s){return(0,t.wg)(),(0,t.iD)("div",null,a)}var s={name:"AdminOrders",data(){return{}}},d=r(3744);const i=(0,d.Z)(s,[["render",c]]);var f=i}}]);
+//# sourceMappingURL=757.9af6ab5e.js.map
